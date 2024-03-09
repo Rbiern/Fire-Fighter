f2a0dca9097e9fe02ded3a4b6700f72a18ca9faf
Chaewon Eom <ceom@uwo.ca>
2024-03-08 23:40:57 -0500

    Added ui.h

---
+++ src/ui.h
@@ -0,0 +1,32 @@
+#ifndef FIRE_FIGHTER_UI_H
+#define FIRE_FIGHTER_UI_H
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include "settings.h"
+
+namespace mainWindow {
+    class ui {
+    // variables
+    private:
+        // required!
+        sf::RenderWindow &_window;
+        settings* options;
+        // optional
+        sf::Image icon;
+        sf::Sprite background;
+        sf::Font font;
+        sf::Music music;
+        bool errorFlag;
+    // methods
+    public:
+        // constructor
+        explicit ui(sf::RenderWindow &window1);
+        void displayMenu();
+    // helper methods
+    private:
+        bool _init();
+    };
+}//end of namespace mainWindow
+#endif //FIRE_FIGHTER_UI_H
