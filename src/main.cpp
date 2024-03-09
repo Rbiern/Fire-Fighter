e48b10ecaa41ee02d69a3b205f070c4f719267e1
Robert Biernacki <156489385+Rbiern@users.noreply.github.com>
2024-03-08 19:22:03 -0500

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "ui.h"
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(720, 480), "Fire Fighter"); //make new window
+    mainWindow::ui program(window); // initialize ui window
+    program.displayMenu(); // method from ui to run the main program loop
+    return 0;
+}
+
+
+
