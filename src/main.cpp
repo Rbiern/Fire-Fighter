f9a55352b468b6974dcdaa581c9ddd905edeee7a
Robert Biernacki <156489385+Rbiern@users.noreply.github.com>
2024-03-09 06:09:49 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(720, 480), "Fire Fighter"); //make new window
+    sf::RenderWindow window(sf::VideoMode(640, 360), "Fire Fighter"); //make new window
     mainWindow::ui program(window); // initialize ui window
     program.displayMenu(); // method from ui to run the main program loop
     return 0;
