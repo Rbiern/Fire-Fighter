455874778cb912b59c0d50d9485d9bc189f937cf
sungmin2ee <160983157+sungmin2ee@users.noreply.github.com>
2024-03-10 03:15:30 -0400

    Update player.h

--- src/player.h
+++ src/player.h
@@ -5,9 +5,24 @@
 #ifndef FIRE_FIGHTER_PLAYER_H
 #define FIRE_FIGHTER_PLAYER_H
 
+#include <SFML/Graphics.hpp>
+#include "entity.h"
 
-class player {
+class Player : public Entity {
+private:
+    float movementSpeed;
 
+public:
+    Player(float startX, float startY);
+
+    void move(const sf::Vector2f& direction);
+    void draw(sf::RenderWindow& window);
+
+    void setSpeed(float speed);
+    float getSpeed() const;
+
+    sf::Vector2f getSize() const;
+    sf::Vector2f getPosition() const;
 };
 
 
