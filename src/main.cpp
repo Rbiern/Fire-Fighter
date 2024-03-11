40969ec32793c62be122fac64e0904e633f152b4
Robert Biernacki <156489385+Rbiern@users.noreply.github.com>
2024-03-11 03:21:50 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,7 @@
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(640, 360), "Fire Fighter"); //make new window
-    mainWindow::ui program(window); // initialize ui window
+    ui program(&window); // initialize ui window
     program.displayMenu(); // method from ui to run the main program loop
     return 0;
 }
-
-
-
