702b830b82c93142f919128aff42d366e47580f0
Robert Biernacki <156489385+Rbiern@users.noreply.github.com>
2024-03-11 03:20:26 -0500

    Update ui.cpp

--- src/ui.cpp
+++ src/ui.cpp
@@ -1,22 +1,23 @@
 #include "ui.h"
 
-namespace mainWindow {
+//namespace mainWindow {
 
     /** constructor */
-    ui::ui(sf::RenderWindow &window1) : _window(window1) {
-        options = settings().getSettings();
+    ui::ui(sf::RenderWindow *window1) : _window(window1) {
+        options = new settings();
         errorFlag = _init();
     }
 
     /** main program loop */
     void ui::displayMenu() {
+//        reEntry:
         if (errorFlag) {
             return;
         }
 
         // Calculate button dimensions based on window size
-        float windowWidth = static_cast<float>(_window.getSize().x);
-        float windowHeight = static_cast<float>(_window.getSize().y);
+        float windowWidth = static_cast<float>(_window->getSize().x);
+        float windowHeight = static_cast<float>(_window->getSize().y);
         float buttonHeight = 55.f;
         float buttonWidth = windowWidth / 5.f; // Adjusted for 3 buttons with spacing
         float buttonOffsetY = 20.f; // Offset from the bottom
@@ -53,40 +54,44 @@ namespace mainWindow {
         button3.setFillColor(sf::Color(54, 207, 213));
         button3.setPosition(startX - buttonWidth, windowHeight - buttonHeight - buttonOffsetY);
 
-        sf::Text text1(options->getLanguage()[0], font, 20);
+        sf::String s1 = options->getLanguage()[0];
+        sf::Text text1(s1, font, 20);
         text1.setFillColor(sf::Color(235, 70, 60));
         text1.setPosition(button1.getPosition().x, button1.getPosition().y + 20);
 
-        sf::Text text2(options->getLanguage()[1], font, 20);
+        sf::String s2 = options->getLanguage()[1];
+        sf::Text text2(s2, font, 20);
         text2.setFillColor(sf::Color(235, 70, 60));
         text2.setPosition(button2.getPosition().x + 5, button2.getPosition().y + 20);
 
-        sf::Text text3(options->getLanguage()[2], font, 20);
+        sf::String s3 = options->getLanguage()[2];
+        sf::Text text3(s3, font, 20);
         text3.setFillColor(sf::Color(235, 70, 60));
         text3.setPosition(button2.getPosition().x + buttonWidth + 20, button3.getPosition().y + 20);
 
-
         sf::Sprite background(backgroundImage);
         background.setTexture(backgroundImage);
-        background.setScale(_window.getSize().x / background.getLocalBounds().width,
-                            _window.getSize().y / background.getLocalBounds().height);
-        _window.setFramerateLimit(60);
+        background.setScale(_window->getSize().x / background.getLocalBounds().width,
+                            _window->getSize().y / background.getLocalBounds().height);
+        _window->setFramerateLimit(60);
         if (options->toggleMusic()) {
             music.play();
         }
         bool startGameFlag = false;
         bool openSettings = false;
+        bool exitFlag = false;
 
         // The main loop starts here
-        while (_window.isOpen()) {
+        while (1) {
             sf::Event event;
-            while (_window.pollEvent(event)) {
+            while (_window->pollEvent(event)) {
                 // check if user has closed the window
                 if (event.type == sf::Event::Closed) {
-                    _window.close();
+                    _window->close();
+                    return;
                 }
                 if (event.type == sf::Event::MouseButtonPressed) {
-                    sf::Vector2i mousePos = sf::Mouse::getPosition(_window);
+                    sf::Vector2i mousePos = sf::Mouse::getPosition(*_window);
                     //game window
                     if (button3.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
                         std::cout << "start game" << std::endl;
@@ -100,6 +105,7 @@ namespace mainWindow {
                     //quit button
                     if (button2.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
                         std::cout << "exit game" << std::endl;
+                        _window->close();
                         return;
                     }
                 }
@@ -116,17 +122,21 @@ namespace mainWindow {
                 }
             }
             if (openSettings) {
-                if (options->toggleMusic()) {
-                    music.stop();
-                }
-                _window.close();
+//                if (options->toggleMusic()) {
+//                    music.stop();
+//                }
+                _window->close();
                 options->openSettings(options->getResolution()[0], options->getResolution()[1]);
-                if (options->toggleMusic()) {
-                    music.play();
-                }
+                // when exited settings
+                exitFlag = true;
+
+                break;
+//                if (options->toggleMusic()) {
+//                    music.play();
+//                }
             }
             // for button mouse hover effect
-            sf::Vector2i mousePos = sf::Mouse::getPosition(_window);
+            sf::Vector2i mousePos = sf::Mouse::getPosition(*_window);
             if (button1.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
                 button1.setFillColor(sf::Color(0, 255, 0, 200)); // Green with transparency
             } else {
@@ -142,15 +152,26 @@ namespace mainWindow {
             } else {
                 button3.setFillColor(sf::Color(54, 207, 213));
             }
-            _window.clear();
-            _window.draw(background);
-            _window.draw(button1);
-            _window.draw(button2);
-            _window.draw(button3);
-            _window.draw(text1);
-            _window.draw(text2);
-            _window.draw(text3);
-            _window.display();
+            _window->clear();
+            _window->draw(background);
+            _window->draw(button1);
+            _window->draw(button2);
+            _window->draw(button3);
+            _window->draw(text1);
+            _window->draw(text2);
+            _window->draw(text3);
+            _window->display();
+        }
+
+        if (exitFlag) {
+            sf::VideoMode fullScreenMode = sf::VideoMode::getDesktopMode();
+            sf::RenderWindow window((options->isFullScreen()) ? fullScreenMode : sf::VideoMode(options->getResolution()[0],options->getResolution()[1]), "Fire Fighter", (options->isFullScreen()) ? sf::Style::Fullscreen : sf::Style::Default);
+            _window = &window;
+
+
+//            goto reEntry;
+            _init();
+            displayMenu();
         }
     }
 
@@ -161,7 +182,7 @@ namespace mainWindow {
             std::cerr << "Failed to load icon" << std::endl;
             return true;
         }
-        _window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr()); // Set the window icon
+        _window->setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr()); // Set the window icon
 
         // load background image for main menu
         if (!backgroundImage.loadFromFile("../../resource/img/menu_background.jpg")) { //"../resource/img/main_menu_image.jpg"
@@ -170,9 +191,21 @@ namespace mainWindow {
         }
 
         // Set fonts
-        if (!font.loadFromFile("../../resource/fonts/Rajdhani-SemiBold.ttf")) {
-            std::cerr << "Failed to load font!" << std::endl;
-            return true;
+        if (options->getLanguage()[0] == L"\uC0C8 \uAC8C\uC784") {
+            if (!font.loadFromFile("../../resource/fonts/NanumMyeongjoBold.ttf")) {
+                std::cerr << "Failed to load font!" << std::endl;
+                return true;
+            }
+        } else if (options->getLanguage()[0] == L"\u65B0\u6E38\u620F") {
+            if (!font.loadFromFile("../../resource/fonts/simplified_Chinese.ttf")) {
+                std::cerr << "Failed to load font!" << std::endl;
+                return true;
+            }
+        } else {
+            if (!font.loadFromFile("../../resource/fonts/Rajdhani-SemiBold.ttf")) {
+                std::cerr << "Failed to load font!" << std::endl;
+                return true;
+            }
         }
 
         // set music for the main menu
@@ -185,4 +218,4 @@ namespace mainWindow {
         return false;
     }
 
-}// end of mainWindow
+//}// end of mainWindow
