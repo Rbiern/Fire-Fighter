f993406e34d036e135e8962a3ff2f47e70ffd3f4
Robert Biernacki <156489385+Rbiern@users.noreply.github.com>
2024-03-12 13:38:31 -0500

    Update ui.h

--- src/ui.h
+++ src/ui.h
@@ -7,27 +7,24 @@
 #include "settings.h"
 #include "game.h"
 
-//namespace mainWindow {
-    class ui {
+class ui {
     // variables
-    private:
-        // required!
-        sf::RenderWindow* _window;
-        settings* options;
-        // optional
-        sf::Image icon;
-        sf::Texture backgroundImage;
-        sf::Font font;
-        sf::Music music;
-        bool errorFlag;
+private:
+    sf::RenderWindow* _window;
+    settings* options;
+    sf::Texture backgroundImage;
+    sf::Font font;
+    sf::Music music;
+    bool errorFlag;
     // methods
-    public:
-        // constructor
-        explicit ui(sf::RenderWindow *window1);
-        void displayMenu();
+public:
+    // constructor
+    explicit ui(sf::RenderWindow *window1);
+    void displayMenu();
     // helper methods
-    private:
-        bool _init();
-    };
-//}//end of namespace mainWindow
+private:
+    sf::ConvexShape createButtonShape(float windowWidth);
+    bool _init();
+};
+
 #endif //FIRE_FIGHTER_UI_H
