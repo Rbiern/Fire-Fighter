ad7cb796c8e63d7684e44a84895565f11503a4dc
Robert Biernacki <156489385+Rbiern@users.noreply.github.com>
2024-03-13 18:13:33 -0500

    Update game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -1,22 +1,43 @@
 #include "game.h"
-
 game::game() {
     options = new settings();
 }
 
-
-
 void game::gameLoop() {
     sf::VideoMode fullScreenMode = sf::VideoMode::getDesktopMode();
-    sf::RenderWindow window((options->isFullScreen()) ? fullScreenMode : sf::VideoMode(options->getResolution()[0],options->getResolution()[1]), "Gamer Moment", (options->isFullScreen() || options->getResolution()[0] >= fullScreenMode.width) ? sf::Style::Fullscreen : sf::Style::Default);
+    sf::RenderWindow window((options->isFullScreen()) ? fullScreenMode : sf::VideoMode(options->getResolution()[0],options->getResolution()[1]), "Fire Fighter", (options->isFullScreen() || options->getResolution()[0] >= fullScreenMode.width) ? sf::Style::Fullscreen : sf::Style::Default);
     sf::Image icon = options->getIcon();
     sf::Clock clock;
     window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr()); // Set the window icon
     window.setFramerateLimit(60);
     sf::Font font = options->getFont();
-
     Player player(window.getSize().x-100.f, window.getSize().y -100.0f);
-    powerup Powerup;
+
+    //*****************************************************************************************
+    //enemy character
+    bool movingRight = true; // Start moving to the right
+    float dropDownStep = 20.f; // How much enemies move down when reaching a screen edge
+    float horizontalMoveSpeed = 1.f; // Adjust based on desired speed
+    int rows = 4; // Number of rows of enemies
+    int columns = 10; // Number of columns of enemies
+    float spacingX = 100.f; // Horizontal spacing between enemies
+    float spacingY = 70.f; // Vertical spacing between enemies
+    float startX = 100.f; // Starting X position for the first enemy
+    float startY = 50.f; // Starting Y position for the first enemy
+    std::vector<std::vector<Enemy>> enemyGrid(rows, std::vector<Enemy>(columns, Enemy(0, 0, window.getSize().x))); // Initialize all enemies
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j) {
+            float positionX = startX + j * (spacingX);
+            float positionY = startY + i * (spacingY);
+            enemyGrid[i][j] = Enemy(positionX, positionY, window.getSize().x);
+            enemyGrid[i][j].setTexture("../../resource/img/fire.png"); // Set the texture for each enemy
+        }
+    }
+    // end of enemy code
+    //*****************************************************************************************
+
+
     //*****************************************************************************************
     // pick character window code start
     // character textures
@@ -146,7 +167,6 @@ void game::gameLoop() {
     // end of chose player window
     //*************************************************************************************************
 
-
     sf::Clock shootCooldown;
     bool canShoot = true;
     float movementSpeed = 0.5f;
@@ -163,6 +183,8 @@ void game::gameLoop() {
     exitButtonText.setPosition(exitButton.getPosition().x + (exitButton.getSize().x - exitButtonText.getLocalBounds().width) / 2,
                                exitButton.getPosition().y + (exitButton.getSize().y - exitButtonText.getLocalBounds().height) / 2);
 
+
+    powerup Powerup;
     // Load life counter textures
     sf::Texture life3Texture;
     sf::Texture life2Texture;
@@ -183,6 +205,11 @@ void game::gameLoop() {
     lifeCounterSprite.setScale(1.5f, 1.5f); // Adjust scale as needed
     lifeCounterSprite.setPosition(15.f, 15.f); // Top left corner of the window
 
+    // The message to display
+    std::string message = "                      Place your AD here                      ";
+    int messageLength = message.length(); int words = 0;
+    // The position at which to start cutting the message
+    int startPos = 0;
 
     sf::Music music;
     if (options->toggleMusic()) {
@@ -202,26 +229,39 @@ void game::gameLoop() {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
-        // Draw life counter
-        int lives = player.getLives();
-        switch (lives) {
-            case 3:
-                lifeCounterSprite.setTexture(life3Texture);
-                break;
-            case 2:
-                lifeCounterSprite.setTexture(life2Texture);
-                break;
-            case 1:
-                lifeCounterSprite.setTexture(life1Texture);
-                break;
-            case 0:
-                lifeCounterSprite.setTexture(life0Texture);
-                break;
-            default:
-                break;
+        bool shouldMoveDown = false;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < columns; ++j) {
+                if (movingRight) {
+                    if (enemyGrid[i][j].getPosition().x + enemyGrid[i][j].getSize().x + horizontalMoveSpeed > window.getSize().x) {
+                        movingRight = false;
+                        shouldMoveDown = true;
+                        break; // Exit the loop since one enemy hit the boundary
+                    }
+                } else {
+                    if (enemyGrid[i][j].getPosition().x - horizontalMoveSpeed < 0) {
+                        movingRight = true;
+                        shouldMoveDown = true;
+                        break; // Exit the loop since one enemy hit the boundary
+                    }
+                }
+            }
+            if (shouldMoveDown) break; // Exit the loop if we need to move down
         }
-        window.draw(lifeCounterSprite);
+
+        if (shouldMoveDown) {
+            for (int i = 0; i < rows; ++i) {
+                for (int j = 0; j < columns; ++j) {
+                    enemyGrid[i][j].move(0, dropDownStep); // Move down
+                }
+            }
+        } else {
+            for (int i = 0; i < rows; ++i) {
+                for (int j = 0; j < columns; ++j) {
+                    enemyGrid[i][j].move((movingRight ? horizontalMoveSpeed : -horizontalMoveSpeed), 0); // Continue moving in the current horizontal direction
+                }
+            }
+        }
 
         // Move character
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
@@ -254,8 +294,40 @@ void game::gameLoop() {
         if (shootCooldown.getElapsedTime().asSeconds() > 0.5f) {
             canShoot = true;
         }
+        int lives = player.getLives();
+        switch (lives) {
+            case 3:
+                lifeCounterSprite.setTexture(life3Texture);
+                break;
+            case 2:
+                lifeCounterSprite.setTexture(life2Texture);
+                break;
+            case 1:
+                lifeCounterSprite.setTexture(life1Texture);
+                break;
+            case 0:
+                lifeCounterSprite.setTexture(life0Texture);
+                break;
+            default:
+                break;
+        }
 
-        // Update powerup
+        player.updateBullets(deltaTime);
+
+        window.clear();
+        if (words == 25) {
+            // Update the window title with a substring of the message
+            std::string title = message.substr(startPos, 100); // Adjust the length based on your needs
+            window.setTitle(title);
+            // Increment the start position to move the text, wrapping around if necessary
+            if (++startPos + 20 > messageLength) startPos = 0;
+            words = 0;
+        } else {
+            words++;
+        }
+
+
+        window.draw(lifeCounterSprite);
         Powerup.update(deltaTime, player);
 
         player.updateBullets(deltaTime);
@@ -266,7 +338,17 @@ void game::gameLoop() {
         player.drawBullets(window);
         window.draw(exitButton);
         window.draw(exitButtonText);
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < columns; ++j) {
+                enemyGrid[i][j].update(deltaTime);
+                enemyGrid[i][j].draw(window);
+            }
+        }
         window.display();
+
+
+
+//        window.display();
     }
     return;
 }
