56cf8c99f8a601c77d60aa33f2cc87522df31d13
chaewoneom <104227958+chaewoneom@users.noreply.github.com>
2024-03-25 17:26:03 -0400

    Update barrier.h

--- src/barrier.h
+++ src/barrier.h
@@ -8,15 +8,15 @@
 class Barrier : public Entity {
 private:
     float size;
-    float initialSize;
     float xPosition;
     float yPosition;
 
 public:
-    // Constructor
     Barrier(float startX, float startY);
+
     bool enemyCollision(const sf::Sprite& enemySprite) const;
     bool bulletCollision(const sf::Sprite& bulletSprite) const;
+
     void shrink();
     void reset();
     void spawn(float newX, float newY);
