4183aa3ad5b7694938c33c1a5b13b2f811aa4ee6
chaewoneom <104227958+chaewoneom@users.noreply.github.com>
2024-03-25 17:25:32 -0400

    Update barrier.cpp

--- src/barrier.cpp
+++ src/barrier.cpp
@@ -3,9 +3,9 @@
 Barrier::Barrier(float startX, float startY) : Entity() {
     this->setTexture("../../resource/img/iceBlock.png");
     this->setPosition(startX, startY);
-
-//    sf::Vector2f scale(size / texture.getSize().x, size / texture.getSize().y);
-//    barrierSprite.setScale(scale);
+    size = 3.5;
+    sf::Vector2f scale(size, size);
+    sprite.setScale(scale);
 }
 
 
@@ -18,19 +18,22 @@ bool Barrier::bulletCollision(const sf::Sprite& bulletSprite) const {
 }
 
 void Barrier::shrink() {
-    size /= 1.2; // Divide the size by 1.2
+    // Calculate the new size
+    size /= 1.02; // Divide the size by 1.02 to shrink it
+
     // Ensure size doesn't go below a minimum value
     if (size < 0) {
-        size = 0;
+        reset();
     }
+
     // Resize the barrier sprite
-    sf::Vector2f scale(size / texture.getSize().x, size / texture.getSize().y);
+    sf::Vector2f scale(size, size); // Assuming initialSize is the original size of the barrier
     sprite.setScale(scale);
 }
 
 void Barrier::reset() {
-    size = initialSize;
-    sf::Vector2f scale(size / texture.getSize().x, size / texture.getSize().y);
+    size = 3.5;
+    sf::Vector2f scale(size, size);
     sprite.setScale(scale);
 }
 
