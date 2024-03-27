c48d257d0943308dc65ac0fa284c1e466cf621d7
chaewoneom <104227958+chaewoneom@users.noreply.github.com>
2024-03-26 23:39:13 -0400

    Update barrier.cpp

--- src/barrier.cpp
+++ src/barrier.cpp
@@ -1,20 +1,13 @@
 #include "barrier.h"
 
-Barrier::Barrier(float startX, float startY) : Entity() {
+Barrier::Barrier(float startX, float startY, const sf::Vector2u& resolution) : Entity() {
     this->setTexture("../../resource/img/iceBlock.png");
     this->setPosition(startX, startY);
-    size = 3.5;
-    sf::Vector2f scale(size, size);
-    sprite.setScale(scale);
+    adjustForResolution(resolution);
 }
 
-
-bool Barrier::enemyCollision(const sf::Sprite& enemySprite) const {
-    return sprite.getGlobalBounds().intersects(enemySprite.getGlobalBounds());
-}
-
-bool Barrier::bulletCollision(const sf::Sprite& bulletSprite) const {
-    return sprite.getGlobalBounds().intersects(bulletSprite.getGlobalBounds());
+bool Barrier::enemyCollision(const Enemy& enemy) const {
+    return sprite.getGlobalBounds().intersects(enemy.getGlobalBounds());
 }
 
 void Barrier::shrink() {
@@ -23,7 +16,7 @@ void Barrier::shrink() {
 
     // Ensure size doesn't go below a minimum value
     if (size < 0) {
-        reset();
+        return;
     }
 
     // Resize the barrier sprite
@@ -31,18 +24,43 @@ void Barrier::shrink() {
     sprite.setScale(scale);
 }
 
-void Barrier::reset() {
-    size = 3.5;
+void Barrier::reset(const sf::Vector2u& resolution) {
+    float referenceWidth = 1920.0f; // Reference width for size calculation
+    float referenceHeight = 1080.0f; // Reference height for size calculation
+
+    // Calculate the ratio of the current resolution to the reference resolution
+    float widthRatio = resolution.x / referenceWidth;
+    float heightRatio = resolution.y / referenceHeight;
+
+    size = 3.5f * std::min(widthRatio, heightRatio);
     sf::Vector2f scale(size, size);
     sprite.setScale(scale);
 }
 
-void Barrier::spawn(float newX, float newY) {
-
-}
+//void Barrier::spawn(float newX, float newY) {
+//
+//}
 
 void Barrier::draw(sf::RenderWindow& window) {
     window.draw(sprite);
+}
+
+void Barrier::adjustForResolution(const sf::Vector2u& resolution) {
+
+    float referenceWidth = 1920.0f; // Reference width for size calculation
+    float referenceHeight = 1080.0f; // Reference height for size calculation
+
+    // Calculate the ratio of the current resolution to the reference resolution
+    float widthRatio = resolution.x / referenceWidth;
+    float heightRatio = resolution.y / referenceHeight;
+
+    // Calculate the size of the barrier based on the resolution ratio
+    size = 3.5f * std::min(widthRatio, heightRatio);
+
+    // Set the scale of the barrier sprite
+    sf::Vector2f scale(size, size);
+    sprite.setScale(scale);
+
 }
 
 sf::Vector2f Barrier::getSize() const {
