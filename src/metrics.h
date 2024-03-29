562f3ef979d8b69da70f10453fcf683e8d546a12
Robert Biernacki <156489385+Rbiern@users.noreply.github.com>
2024-03-29 10:20:48 -0400

    Update metrics.h

--- src/metrics.h
+++ src/metrics.h
@@ -2,29 +2,34 @@
 #define FIRE_FIGHTER_METRICS_H
 
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include <string>
+#include "settings.h"
 
 class Metrics {
 private:
-    sf::Vector2u windowSize;
-    sf::Font font;
-    sf::Text scoreText, pointsText, roundText, enemyCountText, enemyKilledText;
+    sf::RectangleShape infoBar;
     sf::RectangleShape separationLine;
+    sf::Font font;
+    sf::Vector2u windowSize;
+    sf::Text scoreText;
+    sf::Text roundText;
+    sf::Text enemyKilledText;
+    sf::Text enemiesRemainingText;
+    settings* options;
     int score;
-public:
-    Metrics(sf::Vector2u windowSize, sf::Font f);
-    void setScore();
-    void setPoints(int points);
-    void setRound(int round);
-    void setEnemyCount(int enemyCount);
-    void setEnemyKilled(int enemyKilled);
-    void draw(sf::RenderTarget& target);
-    void updateHealthbar(int lives);
+    int enemySum;
     sf::Texture life3Texture;
     sf::Texture life2Texture;
     sf::Texture life1Texture;
     sf::Texture life0Texture;
     sf::Sprite healthBar;
+public:
+    Metrics(sf::Vector2u windowSize, settings* opt);
+    ~Metrics();
+    void draw(sf::RenderTarget& target);
+    void updateHealthbar(int lives);
+    void setEnemyCount(int enemyCount);
     void increaseScore(int points);
     int getScore() const;
 };
