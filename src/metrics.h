ff2b1673d14138bae9831d55359ed5d7c92e9327
JUYOUNG LEE <135635944+Jellyfish135@users.noreply.github.com>
2024-03-30 16:57:17 -0400

    Update metrics.h

--- src/metrics.h
+++ src/metrics.h
@@ -1,37 +1,106 @@
-#ifndef FIRE_FIGHTER_METRICS_H
-#define FIRE_FIGHTER_METRICS_H
+#include "metrics.h"
 
-#include <SFML/Graphics.hpp>
-#include <iostream>
-#include <string>
-#include "settings.h"
+Metrics::Metrics(sf::Vector2u windowSize, settings* opt) : score(0), windowSize(windowSize) {
+    // Calculate the infoBar height as 10% of the window's height
+    float infoBarHeight = windowSize.y * 0.1f;
 
-class Metrics {
-private:
-    sf::RectangleShape infoBar;
-    sf::RectangleShape separationLine;
-    sf::Font font;
-    sf::Vector2u windowSize;
-    sf::Text scoreText;
-    sf::Text roundText;
-    sf::Text enemyKilledText;
-    sf::Text enemiesRemainingText;
-    settings* options;
-    int score;
-    int enemySum;
-    sf::Texture life3Texture;
-    sf::Texture life2Texture;
-    sf::Texture life1Texture;
-    sf::Texture life0Texture;
-    sf::Sprite healthBar;
-public:
-    Metrics(sf::Vector2u windowSize, settings* opt);
-    ~Metrics();
-    void draw(sf::RenderTarget& target);
-    void updateHealthbar(int lives);
-    void setEnemyCount(int enemyCount);
-    void increaseScore(int points);
-    int getScore() const;
-};
+    // Create a infoBar that spans the width of the window and 10% of its height
+    infoBar.setSize(sf::Vector2f(windowSize.x, infoBarHeight));
+    infoBar.setFillColor(sf::Color(100, 100, 100)); // Set the infoBar to be blue
+    infoBar.setPosition(0, 0); // Set the position to the top-left corner
 
-#endif //FIRE_FIGHTER_METRICS_H
+    separationLine.setSize(sf::Vector2f(windowSize.x, 2));
+    separationLine.setFillColor(sf::Color::White);
+    separationLine.setPosition(0, infoBarHeight - 3);
+
+    options = opt;
+    font = opt->getFont();
+
+    scoreText.setFont(font);
+    scoreText.setCharacterSize(opt->heightScaling(30));
+    scoreText.setFillColor(sf::Color::White);
+    scoreText.setString(opt->getLanguage()[22] + std::to_string(score));
+    scoreText.setPosition(((windowSize.x * 0.5f) - (scoreText.getLocalBounds().width) / 2.f), (infoBarHeight/2) - (scoreText.getLocalBounds().height/2));
+
+    roundText.setFont(font);
+    roundText.setCharacterSize(opt->heightScaling(30));
+    roundText.setFillColor(sf::Color::White);
+    roundText.setString(opt->getLanguage()[23]+ std::to_string(score));
+    roundText.setPosition((((windowSize.x/2) * 0.6f) - (roundText.getLocalBounds().width) / 2.f), (infoBarHeight/2) - (roundText.getLocalBounds().height/2));
+
+    enemyKilledText.setFont(font);
+    enemyKilledText.setCharacterSize(opt->heightScaling(30));
+    enemyKilledText.setFillColor(sf::Color::White);
+    enemyKilledText.setString(opt->getLanguage()[24] + std::to_string(score));
+    enemyKilledText.setPosition((((windowSize.x/2) * 1.25f) - (enemyKilledText.getLocalBounds().width) / 2.f), (infoBarHeight/2) - (enemyKilledText.getLocalBounds().height/2));
+
+    enemiesRemainingText.setFont(font);
+    enemiesRemainingText.setCharacterSize(opt->heightScaling(30));
+    enemiesRemainingText.setFillColor(sf::Color::White);
+    enemiesRemainingText.setString(opt->getLanguage()[25] + std::to_string(score));
+    enemiesRemainingText.setPosition((((windowSize.x/2) * 1.7f) - (enemiesRemainingText.getLocalBounds().width) / 2.f), (infoBarHeight/2) - (enemiesRemainingText.getLocalBounds().height/2));
+
+
+    if (!life3Texture.loadFromFile("../../resource/img/3.png") ||
+        !life2Texture.loadFromFile("../../resource/img/2.png") ||
+        !life1Texture.loadFromFile("../../resource/img/1.png") ||
+        !life0Texture.loadFromFile("../../resource/img/0.png")) {
+        std::cerr << "Failed to load life counter textures!" << std::endl;
+        return;
+    }
+    healthBar.setTexture(life3Texture);
+    healthBar.setScale(opt->widthScaling(1.4), opt->widthScaling(1.4));
+    healthBar.setPosition(windowSize.x * 0.03, (infoBarHeight/2) - (life3Texture.getSize().y / 2));
+}
+
+Metrics::~Metrics() {
+
+}
+
+void Metrics::updateHealthbar(int lives){
+    switch (lives) {
+        case 3:
+            healthBar.setTexture(life3Texture);
+            break;
+        case 2:
+            healthBar.setTexture(life2Texture);
+            break;
+        case 1:
+            healthBar.setTexture(life1Texture);
+            break;
+        case 0:
+            healthBar.setTexture(life0Texture);
+            break;
+        default:
+            break;
+    }
+}
+
+void Metrics::draw(sf::RenderTarget& target) {
+    target.draw(infoBar);
+    target.draw(separationLine);
+    target.draw(scoreText);
+    target.draw(roundText);
+    target.draw(healthBar);
+    target.draw(enemyKilledText);
+    target.draw(enemiesRemainingText);
+}
+
+void Metrics::setEnemyCount(int enemyCount) {
+    enemySum = enemyCount;
+    enemiesRemainingText.setString(options->getLanguage()[25] + std::to_string(enemyCount));
+}
+
+void Metrics::increaseScore(int points) {
+    score += points;
+    scoreText.setString(options->getLanguage()[22] + std::to_string(score));
+    roundText.setString(options->getLanguage()[23]+ std::to_string(enemySum/ (score/10)));
+    enemyKilledText.setString(options->getLanguage()[24] + std::to_string(score/points));
+    enemiesRemainingText.setString(options->getLanguage()[25] + std::to_string(enemySum--));
+}
+
+int Metrics::getScore() const {
+    return score;
+}
+
+
