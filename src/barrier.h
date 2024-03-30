3b1fd69bef73fc00071a554c3eb53b4815d3c2e3
chaewoneom <104227958+chaewoneom@users.noreply.github.com>
2024-03-30 02:28:54 -0400

    Update barrier.h

--- src/barrier.h
+++ src/barrier.h
@@ -3,19 +3,20 @@
 
 #include "entity.h"
 #include "enemy.h"
+#include "bullet.h"
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
 class Barrier : public Entity {
 private:
     float size;
-    float xPosition;
-    float yPosition;
+//    float xPosition;
+//    float yPosition;
     float barrierSpacing;
 
-public:
-    Barrier(float startX, float startY, const sf::Vector2u& resolution);
 
+public:
+    Barrier(float startX, float startY, sf::RenderWindow& window, const sf::Vector2u& resolution);
     bool enemyCollision(const Enemy& enemy) const;
     bool bulletCollision(const sf::Sprite& bulletSprite) const;
 
@@ -25,12 +26,14 @@ public:
     void draw(sf::RenderWindow& window);
     void adjustForResolution(const sf::Vector2u& resolution);
     float adjustSpacingForResolution(const sf::Vector2u& resolution);
+    void setUpBarrier(sf::RenderWindow& window, const sf::Vector2u& resolution);
+    void updateBarrier(EnemyWave enemyWave, const sf::Vector2u& resolution);
 
+    std::vector<Barrier> barriers;
     sf::Vector2f getSize() const;
     sf::Vector2f getPosition() const;
 
     virtual ~Barrier();
-
 
 };
 
@@ -38,10 +41,14 @@ public:
 //private:
 //    int barrierRows;
 //    int barrierColumns;
+//    float size;
 //    float barrierSpacing;
+//    std::vector<std::vector<Barrier>> barriers;
 //public:
 //    BarrierWave(sf::RenderWindow& window, const sf::Vector2u& resolution);
+//    void adjustForResolution(const sf::Vector2u& resolution);
 //    float adjustSpacingForResolution(const sf::Vector2u& resolution);
+//    void draw(sf::RenderWindow& window);
 //};
 
 #endif //FIRE_FIGHTER_BARRIER_H
