947a89be7053c7f2ca4817d93a9853275c65df96
Prachi Ghevaria <162646052+Prachighev@users.noreply.github.com>
2024-03-29 20:56:51 -0400

    Update bullet.cpp

--- src/bullet.cpp
+++ src/bullet.cpp
@@ -1,14 +1,20 @@
+#include <iostream>
 #include "Bullet.h"
 
-Bullet::Bullet(float startX, float startY) {
-    loadTexture();
+Bullet::Bullet(float startX, float startY, const std::string& type) {
+    loadTexture(type);
     sprite.setPosition(startX, startY);
     sprite.setScale(0.2f,0.2f);
     speed = 300.f;
 }
 
-void Bullet::update(const sf::Time& delta) {
-    sprite.move(-speed * delta.asSeconds(), 0);
+void Bullet::update(const sf::Time& delta, const std::string& type) {
+    if (type == "enemy"){
+        sprite.move(speed * delta.asSeconds(), 0);
+    }
+    else if(type == "player"){
+        sprite.move(-speed * delta.asSeconds(), 0);
+    }
 }
 
 void Bullet::draw(sf::RenderWindow& window) {
@@ -19,11 +25,16 @@ sf::FloatRect Bullet::getGlobalBounds() const {
     return sprite.getGlobalBounds();
 }
 
-void Bullet::loadTexture() {
-    if (!texture.loadFromFile("../../resource/img/water-bullet.png")) {
+void Bullet::loadTexture(const std::string &type) {
+    if (type =="player" && !texture.loadFromFile("../../resource/img/water-bullet.png")) {
+        std::cerr << "Failed to load player's bullet texture" << std::endl;
+    }
+    else if(type =="enemy" && !texture.loadFromFile("../../resource/img/fireball.png")) {
+        std::cerr << "Failed to load enemy's bullet texture" << std::endl;
     }
     sprite.setTexture(texture);
 }
+
 const sf::Sprite& Bullet::getSprite() const {
     return sprite;
 }
