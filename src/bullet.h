fdc563e46757deafa9de8cf9d5ad165ba24f9724
Prachi Ghevaria <162646052+Prachighev@users.noreply.github.com>
2024-03-29 20:58:54 -0400

    Update bullet.h

--- src/bullet.h
+++ src/bullet.h
@@ -5,8 +5,8 @@
 
 class Bullet {
 public:
-    Bullet(float startX, float startY);
-    void update(const sf::Time& delta);
+    Bullet(float startX, float startY, const std::string &type);
+    void update(const sf::Time& delta, const std::string &type);
     void draw(sf::RenderWindow& window);
     sf::FloatRect getGlobalBounds() const;
     const sf::Sprite &getSprite() const;
@@ -16,7 +16,7 @@ private:
     sf::Texture texture;
     float speed;
 
-    void loadTexture();
+    void loadTexture(const std::string& type);
     //const sf::Sprite &getSprite() const;
 };
 
