47a38bf7ded3ce3295bf01494d1de44cb3719db4
JUYOUNG LEE <135635944+Jellyfish135@users.noreply.github.com>
2024-04-01 17:54:24 -0400

    Update enemywave.h

--- src/enemywave.h
+++ src/enemywave.h
@@ -1,3 +1,11 @@
+/**
+ * @file Enemywave.h
+ * @brief Declaration of the Enemywave class.
+ */
+
+#ifndef FIRE_FIGHTER_ENEMYWAVE_H
+#define FIRE_FIGHTER_ENEMYWAVE_H
+
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <vector>
@@ -7,8 +15,25 @@
 #include <cmath>
 #include <iostream>
 
+/**
+ * @class Enemywave
+ * @brief Represents a enemy movement object in the game.
+ *
+ * This class is responsible for creating, managing, and updating enemy waves within the game. It handles the
+ * initialization of enemies based on the game window size and resolution, and provides functionality to update
+ * enemy positions and behaviors over time.
+ */
 class EnemyWave {
+
 public:
+    /**
+     *Constructor: Initializes an enemy wave within the game environment, setting up initial positions, spacing,
+     *and creating enemies based on the game's window size, resolution, and UI metrics bar height.
+
+    * @param window
+     * @param resolution
+     * @param metricsBarHeight
+     */
     EnemyWave(sf::RenderWindow& window, const sf::Vector2u& resolution, float metricsBarHeight);
     void update(sf::Time deltaTime);
     void draw(sf::RenderWindow& window);
@@ -40,3 +65,5 @@ private:
     float startY;
     int totalSpawned = 0;
 };
+
+#endif FIRE_FIGHTER_ENEMYWAVE_H
