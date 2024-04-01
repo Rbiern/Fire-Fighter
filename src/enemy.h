825bf7ae3096723c7a6a13c4ebd126838201820e
JUYOUNG LEE <135635944+Jellyfish135@users.noreply.github.com>
2024-04-01 15:37:31 -0400

    Update enemy.h

--- src/enemy.h
+++ src/enemy.h
@@ -37,36 +37,3 @@ private:
     sf::Vector2u res1;
 };
 
-class EnemyWave {
-public:
-    EnemyWave(sf::RenderWindow& window, const sf::Vector2u& resolution, float metricsBarHeight);
-    void update(sf::Time deltaTime);
-    void draw(sf::RenderWindow& window);
-    void removeEnemy(int row, int column);
-    Enemy& getEnemy(int row, int column);
-    int getRows() const;
-    int getColumns() const;
-    void adjustSpacingForResolution(const sf::Vector2u& resolution);
-    sf::RenderWindow& window;
-    void respawnEnemies();
-    bool allEnemiesDead() const;
-
-    //enemy count
-    void enemyHit(int row, int column); // Method to call when an enemy is hit
-    int getTotalSpawned() const { return totalSpawned; }
-    void update(sf::Time deltaTime, float metricsBarHeight);
-
-
-private:
-    std::vector<std::vector<Enemy>> enemyGrid;
-    float waveAmplitude;
-    float waveFrequency;
-    float wavePhase;
-    int rows;
-    int columns;
-    float spacingX;
-    float spacingY;
-    float startX;
-    float startY;
-    int totalSpawned = 0;
-};
