0857b18980926f3f76cd8860a17b1dcd9f3e9451
Prachi Ghevaria <162646052+Prachighev@users.noreply.github.com>
2024-04-01 05:26:49 -0400

    Update powerup.h

--- src/powerup.h
+++ src/powerup.h
@@ -14,12 +14,16 @@ private:
     sf::Vector2f direction;
     bool collected;
     std::mt19937 gen;
+    sf::Vector2u resolution;
+    settings gameSettings;
+
 public:
     powerup();
     ~powerup();
     void reset();
     void update(const sf::Time& delta, Player* player, sf::RenderWindow& window);
     void draw(sf::RenderWindow& window, Player* player);
+    void adjustForResolution(const sf::Vector2u& resolution);
 };
 
 #endif //FIRE_FIGHTER_POWERUP_H
