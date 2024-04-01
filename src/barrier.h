25df4f6ea1fa012929a83fa98508d80131b2f752
Robert Biernacki <156489385+Rbiern@users.noreply.github.com>
2024-04-01 04:46:53 -0400

    Update barrier.h

--- src/barrier.h
+++ src/barrier.h
@@ -1,46 +1,25 @@
 #ifndef FIRE_FIGHTER_BARRIER_H
 #define FIRE_FIGHTER_BARRIER_H
 
-#include "entity.h"
-#include "enemy.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
 #include "bullet.h"
 #include "settings.h"
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-#include <iostream>
 
-class Barrier : public Entity {
+class Barrier {
 private:
-    float size;
-    float barrierX;
-    float barrierY;
-    float scaleFactor;
-    float barrierSpacing;
-    float minimumSize;
-    const float referenceWidth;
-    const float referenceHeight;
     settings gameSettings;
-    sf::Vector2u resolution;
-    sf::SoundBuffer shrinkBuffer;
-    sf::Sound shrinkSound;
-
+    sf::Texture iceBlockTexture;
+    sf::Sprite iceBlockSprite;
+    float size;
 public:
-    Barrier(float barrierX, float barrierY, sf::RenderWindow& window, settings gameSettings);
-    bool bulletCollision(const sf::Sprite& bulletSprite) const;
-
-    void shrink();
-    void reset(const sf::Vector2u& resolution);
-    void spawn(float newX, float newY);
-    void draw(sf::RenderWindow& window);
-    float getBarrierSpacing();
-    void adjustForResolution(const sf::Vector2u& resolution);
-    void adjustSpacingForResolution(const sf::RenderWindow& window, const sf::Vector2u& resolution);
-
-    std::vector<Barrier> barriers;
-    sf::Vector2f getSize() const;
-    sf::Vector2f getPosition() const;
-
+    explicit Barrier(settings gameSettings);
     virtual ~Barrier();
+    void setPosition(int posX, int posY);
+    bool bulletCollision(const sf::Sprite& bulletSprite) const;
+    void shrink();
+    void reset();
+    void draw(sf::RenderWindow& window);
 };
 
 #endif //FIRE_FIGHTER_BARRIER_H
