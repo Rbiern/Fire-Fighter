f46921f657c9f5e793a94bff750ea0dfe23ebd01
JUYOUNG LEE <135635944+Jellyfish135@users.noreply.github.com>
2024-04-01 17:54:07 -0400

    Update enemy.h

--- src/enemy.h
+++ src/enemy.h
@@ -1,35 +1,31 @@
-#pragma once
+#ifndef FIRE_FIGHTER_ENEMY_H
+#define FIRE_FIGHTER_ENEMY_H
+
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include <string>
-#include "bullet.h"
-#include <cmath>
 #include <iostream>
+#include "bullet.h"
+#include "entity.h"
 
-class Enemy {
+class Enemy : public Entity {
 public:
     Enemy(float startX, float startY, unsigned int screenWidth, const sf::Vector2u& resolution);
     void update(const sf::Time& deltaTime);
     void draw(sf::RenderWindow& window);
     void setTexture(const std::string& texturePath);
-    sf::Vector2f getPosition() const;
-    void setPosition(float x, float y);
     sf::Vector2u getSize() const;
     sf::FloatRect getGlobalBounds() const;
     void kill();
     bool getIsAlive() const;
     static int getTotalDeath();
-    const sf::Sprite &getSprite() const;
-    void shoot(); // Function to make the enemy shoot
-    std::vector<Bullet>& getBullets(); // Function to get the bullets shot by the enemy
-    void setIsAlive(bool alive); // Set alive status
-    void increaseSpeed(float factor); // Increase movement speed
-    bool hasMovedRight = false;
+    void shoot();
+    std::vector<Bullet>& getBullets();
+    void setIsAlive(bool alive);
+    void increaseSpeed(float factor);
     void removeBullet();
 
 private:
-    sf::Sprite sprite;
-    sf::Texture texture;
     std::vector<Bullet> bullets;
     float movementSpeed;
     unsigned int screenWidth;
@@ -38,3 +34,4 @@ private:
     sf::Vector2u res1;
 };
 
+#endif // FIRE_FIGHTER_ENEMY_H
