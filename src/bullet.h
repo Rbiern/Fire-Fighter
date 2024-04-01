3837682237cdb83b2c8c396d62e2e1ba5156362b
Robert Biernacki <156489385+Rbiern@users.noreply.github.com>
2024-04-01 05:41:31 -0400

    Update bullet.h

--- src/bullet.h
+++ src/bullet.h
@@ -2,26 +2,21 @@
 #define FIRE_FIGHTER_BULLET_H
 
 #include <SFML/Graphics.hpp>
-#include "settings.h"
+#include <iostream>
 
 class Bullet {
+private:
+    sf::Sprite bulletSprite;
+    sf::Texture bulletTexture;
+    float speed;
+    void loadTexture(const std::string& type);
 public:
-    Bullet(float startX, float startY, const std::string &type);
+    Bullet(float startX, float startY, const std::string &type, sf::Vector2u res);
+    ~Bullet();
     void update(const sf::Time& delta, const std::string &type);
     void draw(sf::RenderWindow& window, const std::string &type);
-    void adjustForResolution(const sf::Vector2u& resolution);
     sf::FloatRect getGlobalBounds() const;
     const sf::Sprite &getSprite() const;
-
-private:
-    sf::Sprite sprite;
-    sf::Texture texture;
-    sf::Vector2u resolution;
-    settings gameSettings;
-    float speed;
-
-    void loadTexture(const std::string& type);
-    //const sf::Sprite &getSprite() const;
 };
 
 #endif //FIRE_FIGHTER_BULLET_H
