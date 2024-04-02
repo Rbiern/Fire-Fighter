6f52cbf4d451e8d7eae59f47c0633699c8f78b78
sungmin2ee <160983157+sungmin2ee@users.noreply.github.com>
2024-04-01 22:09:27 -0400

    Update entity.cpp

--- src/entity.cpp
+++ src/entity.cpp
@@ -1,96 +1,32 @@
-#include "enemy.h"
+#include "entity.h"
 
-/**
- *
- * @param startX
- * @param startY
- * @param screenWidth
- * @param resolution
- */
-Enemy::Enemy(float startX, float startY, unsigned int screenWidth, const sf::Vector2u& resolution)
-        : movementSpeed(3.0f), screenWidth(screenWidth),isAlive(true) {
-    setPosition(startX, startY);
-    res1.x = resolution.x;
-    res1.y = resolution.y;
+Entity::Entity() {}
 
-    sprite.setScale(1.f * ((float)resolution.x/1280.f), 1.f * ((float)resolution.y/720.f));
-    movementSpeed *= (1.0f * ((float)resolution.x/1280.f));
+
+void Entity::move(float deltaX, float deltaY) {
+    sprite.move(deltaX, deltaY);
 }
 
-
-int Enemy::totalDeath = 0;
-
-void Enemy::kill() {
-    isAlive = false;
-    totalDeath++;
-    std::cout << "Enemy killed. Total deaths: " << totalDeath << std::endl;
+sf::Sprite& Entity::getSprite() {
+    return sprite;
 }
 
-void Enemy::update(const sf::Time& deltaTime) {
-    float moveDistance = movementSpeed * deltaTime.asSeconds();
-    sprite.move(moveDistance, 0);
-    if (sprite.getPosition().x > screenWidth) {
-        sprite.setPosition(-sprite.getGlobalBounds().width, sprite.getPosition().y);
+sf::Vector2f Entity::getPosition() const {
+    return sprite.getPosition();
+}
+void Entity::setPosition(float x, float y) {
+    sprite.setPosition(x, y);
+}
+void Entity::setScale(float scaleX, float scaleY) {
+    sprite.setScale(scaleX, scaleY);
+}
+bool Entity::setTexture(const std::string& texturePath) {
+    if (texture.loadFromFile(texturePath)) {
+        sprite.setTexture(texture);
+        return true;
     }
-    // Update the position of enemy bullets
-    for (auto& bullet : bullet) {
-        bullet.update(deltaTime,"enemy");
-    }
-    //if bullet goes off screen
-    removeBullet();
+    return false;
 }
-
-void Enemy::draw(sf::RenderWindow& window) {
-    if (isAlive) {
-        window.draw(sprite);
-    }
-    // Draw enemy bullets on the screen
-    for (auto& bullet : bullet) {
-        bullet.draw(window, "enemy");
-    }
-}
-
-void Enemy::setTexture(const std::string& texturePath) {
-    texture.loadFromFile(texturePath);
-    sprite.setTexture(texture);
-}
-
-
-
-sf::FloatRect Enemy::getGlobalBounds() const {
-    return sprite.getGlobalBounds();
-}
-
-
-sf::Vector2u Enemy::getSize() const {
-    return texture.getSize();
-}
-
-bool Enemy::getIsAlive() const {
-    return isAlive; // Assuming 'isAlive' is a boolean member variable indicating the alive status.
-}
-
-void Enemy::shoot() {
-    // Create a new bullet at the position of the enemy
-    Bullet enemyBullet(getPosition().x, getPosition().y, "enemy", res1);
-    bullet.push_back(enemyBullet);
-}
-
-std::vector<Bullet>& Enemy::getBullets() {
-    return bullet;
-}
-
-void Enemy::setIsAlive(bool alive) {
-    isAlive = alive;
-}
-
-void Enemy::increaseSpeed(float factor) {
-    movementSpeed *= factor;
-}
-void Enemy::removeBullet() {
-    for (int i = bullet.size() - 1; i >= 0; --i) {
-        if (bullet[i].getGlobalBounds().getPosition().x > res1.x) {
-            bullet.erase(bullet.begin() + i);
-        }
-    }
+void Entity::draw(sf::RenderWindow& window) const {
+    window.draw(sprite);
 }
