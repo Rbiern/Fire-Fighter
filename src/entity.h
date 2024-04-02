f6258b1876078036d64122a9755f3add6e94bbb6
sungmin2ee <160983157+sungmin2ee@users.noreply.github.com>
2024-04-01 22:03:46 -0400

    Update entity.h

--- src/entity.h
+++ src/entity.h
@@ -1,31 +1,46 @@
-#ifndef FIRE_FIGHTER_ENTITY_H
-#define FIRE_FIGHTER_ENTITY_H
+#ifndef FIRE_FIGHTER_ENEMY_H
+#define FIRE_FIGHTER_ENEMY_H
 
-#include <SFML/Graphics.hpp>
+#include <vector>
+#include <string>
+#include <iostream>
 #include "bullet.h"
-
-
-class Entity {
-protected:
-    sf::Sprite sprite;
-    sf::Texture texture;
-    std::vector<Bullet>  bullet;
-
+#include "entity.h"
+/**
+ * @class Enemy
+ * @brief Manages the enemy's behavior and state in the game.
+ * The Enemy class is responsible for set texture, draw enemy on win
+ *
+ */
+class Enemy : public Entity {
 public:
-    Entity();
-    virtual ~Entity() {}
+    /**
+     * @
+     * @param startX
+     * @param startY
+     * @param screenWidth
+     * @param resolution
+     */
+    Enemy(float startX, float startY, unsigned int screenWidth, const sf::Vector2u& resolution);
+    void update(const sf::Time& deltaTime);
+    void draw(sf::RenderWindow& window);
+    void setTexture(const std::string& texturePath);
+    sf::Vector2u getSize() const;
+    sf::FloatRect getGlobalBounds() const;
+    void kill();
+    bool getIsAlive() const;
+    void shoot();
+    std::vector<Bullet>& getBullets();
+    void setIsAlive(bool alive);
+    void increaseSpeed(float factor);
+    void removeBullet();
 
-    void setPosition(float x, float y);
-    sf::Vector2f getPosition() const;
-
-    void move(float deltaX, float deltaY);
-
-    sf::Sprite& getSprite();
-
-    void draw(sf::RenderWindow& window) const;
-    bool setTexture(const std::string& texturePath);
-
-    void setScale(float scaleX, float scaleY);
+private:
+    float movementSpeed;
+    unsigned int screenWidth;
+    bool isAlive;
+    static int totalDeath;
+    sf::Vector2u res1;
 };
 
-#endif //FIRE_FIGHTER_ENTITY_H
+#endif // FIRE_FIGHTER_ENEMY_H
